3cdbdc528d5f0f48f527a332ff9c2d3baca7063b
michael3335 <mickyharr@gmail.com>
2025-12-01 13:21:15 +1100

    D

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Jost:wght@300;400;500;600;700&family=Space+Grotesk:wght@300..700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className="font-sans bg-white text-black">
         {children}
       </body>
